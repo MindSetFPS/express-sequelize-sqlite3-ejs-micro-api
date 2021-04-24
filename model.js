a9e06b07dfcb2db1fa0c3f736edfcf4132f66e9c
Daniel <daanielescobar@gmail.com>
2021-04-24 02:43:50 -0500

    Added "Pedido" and "Pedido" models.

--- model.js
+++ model.js
@@ -1,5 +1,14 @@
-const { DataTypes } = require('sequelize')
+const { DataTypes, Sequelize, ForeignKeyConstraintError } = require('sequelize')
 const db = require('./db')
+
+/*
+
+------- COMIDA -------
+id      Name        description     link
+INT     STRING      STRING          STRING
+1       awa         joder q sed     awa.com
+
+*/
 
 const Food = db.define('food', {
     id: {
@@ -19,6 +28,87 @@ const Food = db.define('food', {
     }
 })
 
-Food.sync().then(() => console.log('table created'))
+/*
 
-module.exports = Food;
+------  Pedido -------
+idPedido!       comprador!      entregado!      total!      pagado!     fecha!      ubicacion?    
+INT             STRING          BOOLEAN         FLOAT       BOOLEAN     DATE        STRING
+3123123                         
+
+*/
+
+const Pedido = db.define('pedido',{
+    id: {
+      type: DataTypes.UUIDV4,
+      unique: true,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: Sequelize.UUIDV4
+
+    },
+    customer: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    delivered: {
+        type: DataTypes.BOOLEAN,
+        allow: false
+    },
+    total: {
+        type: DataTypes.FLOAT
+    },
+    paid: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    location: {
+        type: DataTypes.STRING
+    }
+})
+
+/*
+------- ORDER ITEM --------
+idPedidoFK  idComidaFK      cantidad
+INT         INT             INT
+5           12-takitos-     3
+
+*/
+
+const PedidoItem = db.define('pedidoItem', {
+    pedidoId: {
+        type: DataTypes.UUIDV4,
+        references: {
+            model: Pedido,
+            key: 'id'
+        },
+    FoodId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Food,
+            key: 'id'
+        }
+    },
+    quantity: {
+        type: DataTypes.FLOAT
+    }
+    }
+})
+
+
+/*
+
+------- MENU DEL DIA --------
+
+*/
+
+Food.sync().then(() => console.log('Food created'))
+Pedido.sync().then(() => console.log('Pedido created'))
+PedidoItem.sync().then(() => console.log('PedidoItem created'))
+
+
+
+module.exports = {
+    Food,
+    Pedido,
+    PedidoItem
+};
