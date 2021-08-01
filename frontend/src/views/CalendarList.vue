fe23c2e391c068e727a3fb312dc532f3d8e7d94e
Daniel <daanielescobar@gmail.com>
2021-07-31 22:21:52 -0500

    Delete calendars. Manage what to display when there are no calendars.

--- frontend/src/views/CalendarList.vue
+++ frontend/src/views/CalendarList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" >
         <h1 class="title" > Calendario </h1>
-            <div  class="mb-extra" >
+            <div v-if="calendars && calendars.length > 0 " class="mb-extra" >
 
                 <div class="card mt-4 mb-4" v-for="calendar in calendars" :key="calendar.id" >
                     <h1 style="text-align: center;"  >  {{ 'Hoy ? ' }} </h1> 
@@ -17,24 +17,33 @@
                         </div>
                         <div class="stack" >
                             <form>
-                                <input class="button secondary love" value="Borrar" formaction="/calendar/delete/{{menu.id}}" formmethod="GET" type="submit" >  
+                                <button class="button secondary love" @click="deleteCalendar(calendar.id)" > Borrar </button>  
                             </form>
                             <button class="button secondary blue" > Editar </button>
                         </div>
                     </div>
                     
                 </div>
+
+            </div>
+
+            <div v-else >
+                <error-alert :message="calendars.message" :link="calendars.link" />
             </div>
     </div>
 </template>
 
 <script>
+import ErrorAlert from '@/components/ErrorAlert.vue'
 export default {
     name: 'CalendarList',
+    components: {
+        ErrorAlert
+    },
     data() {
         return{
             api: process.env.VUE_APP_API,
-            calendars: ''
+            calendars: '',
         }
     },
     methods: {
@@ -43,6 +52,16 @@ export default {
             this.calendars = list
 
             console.log(this.calendars)
+        },
+        async deleteCalendar(calendarId){
+            const toDelete = this.api + '/calendar/api/delete/' + calendarId
+            console.log(toDelete)
+
+            const deleteCalendar = await fetch(toDelete).then(res => res.json()).catch(e => console.error(e))
+
+            if (deleteCalendar.ok){
+                this.getCalendars()
+            }
         }
 
     },
