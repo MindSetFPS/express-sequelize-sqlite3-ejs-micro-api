e05a1109d954beb7cb92ae586c677464b4beb988
Daniel <daanielescobar@gmail.com>
2021-07-31 22:33:17 -0500

    Make ErrorAlert.vue reusable.

--- frontend/src/components/ErrorAlert.vue
+++ frontend/src/components/ErrorAlert.vue
@@ -1,14 +1,32 @@
 <template>
-        <div class="card love " style="display: block; align-items: center; font-size: 16px; vertical-align: middle;"  >
+        <div class="card love alert" >
+            <span class="text">
+                {{ message }}
+            </span>
+            
+
         <span class="material-icons" style="font-size: 18px; vertical-align: middle;" >&#xe000;</span> 
-            <a href="/calendar/create-menu" >
+            <router-link :to="link" >
                 <span class="material-icons" style="font-size: 18px; vertical-align: middle; color: cornflowerblue;" >&#xe895;</span>
-            </a>
+            </router-link>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'ErrorAlert'
+    name: 'ErrorAlert',
+    props: ['link', 'message']
 }
 </script>
+
+<style scoped>
+    .alert{
+        display: flex; 
+        align-items: center; 
+        font-size: 16px; 
+        vertical-align: middle;
+    }
+    .text{
+        font-size: 32px;
+    }
+</style>
