c937dae9cc8c33e5136aada79d6c098525135521
Daniel <daanielescobar@gmail.com>
2021-08-11 18:13:24 -0500

    Change deserialization Change login strategy

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -8,36 +8,50 @@ passport.serializeUser(function(user, done){
     done(null, user.id)
 })  
 
-passport.deserializeUser(function(id, done){
-    done(null, {id: 1, name: "cody"})
+passport.deserializeUser(async function(id, done){
+    user = await User.findOne({
+        where: {
+            id: id
+        }
+    }).catch(e => console.error(e))
+    console.log(user)
+    if(!user){
+        done(null, false)
+    }
+    done(null, user)
 })
 
 
 passport.use('login', new PassportStrategy({usernameField: 'email'}, async function(username, password, done){
-
     user = await User.findOne({
         where: {
             email: username
         }
-    })
-    .catch( e => console.error(e))
-    if( user && user.email == username){        
-        return done(null, user)
+    }).catch( e => console.error(e))
+
+    console.log(user)
+
+    if( user && user.email == username){       
+        if(user.password == password){
+            console.log('Succesfull')
+            return done(null, user)
+        }
+        console.log('Incorrect passord')
+        return done(null, false, {message: 'Incorrect password'})
     }
-    return done(null, false)   
+    console.log('User not found')
+    return done(null, false, {message: 'User not found'})   
 }))
 
 passport.use('register', new PassportStrategy({usernameField: 'email'}, async function(username, password, done){
+
+    console.log(username, password, done )
 
     email = await User.findOne({
         where: {
             email: username
         }
     }).catch(e => console.error(e))
-
-
-    console.log(email)
-
     if(email){
         return done(null, false)
     }
