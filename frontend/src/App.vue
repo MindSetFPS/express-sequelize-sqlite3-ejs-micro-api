98382de7c95ea614c8753b02a999ea7c08152e08
Daniel <daanielescobar@gmail.com>
2021-08-11 18:06:01 -0500

    Manage login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="father" >  
-      <Navbar />
-      <router-view/>
-  </div>
+  <div :class="[user ? 'father' : 'container']" >  
+        <Navbar v-if="user" />
+      <router-view v-on:logInSuccesfull="doSomething" />
+    </div>
 </template>
 
 <script>
@@ -12,6 +12,34 @@ import Navbar from "@/components/Navbar";
 export default {
   components: {
     Navbar
+  },
+  data(){
+    return{
+      user: '',
+      api: process.env.VUE_APP_API
+    }
+  },
+  methods: {
+    async getUsers(){
+      const users = await fetch(this.api + '/accounts/api/get').then(res => res.json())
+      this.users = users
+      console.log(this.users)
+      if(!this.users.exists){
+        console.log('No users found, creating the first user.')
+        this.$router.push('/register')
+      }else{
+        console.log('Users found, redirecting to login.')
+        this.$router.push('/login')
+      }
+      console.log(this.users)
+    },
+    doSomething(payload){
+      this.user = payload
+      console.log(payload)
+    }
+  },
+  mounted(){
+    this.getUsers()
   }
 }
 </script>
