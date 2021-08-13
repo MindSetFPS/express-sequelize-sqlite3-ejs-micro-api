2cdcd454059f1c032e04712f73256f416a91373e
Daniel <daanielescobar@gmail.com>
2021-08-13 12:12:34 -0500

    Check if there are any users in DB

--- src/Accounts/Accounts.js
+++ src/Accounts/Accounts.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const User = require('../auth/UserModel')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    console.log('sending')
-    res.render('accounts.njk')
+router.get('/api/get', async (req, res)=> {
+    console.log('New request to /accounts/api/get')
+    const usersExist = await User.findOne().catch( e => console.error(e))
+
+    console.log(req.user)
+
+    if(!usersExist){
+        console.log(usersExist)
+        res.json({ok: true, exists: false})
+    }else{
+        res.json({ok: true, exists: true})
+    }
+
 })
 
 module.exports = router;
