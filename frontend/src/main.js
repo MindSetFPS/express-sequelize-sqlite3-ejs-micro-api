d9cbd521ae158d8f17c34020def5704732323f86
Daniel <daanielescobar@gmail.com>
2021-10-12 21:44:35 -0500

    Use mdi icons.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,31 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+import mdiVue from 'mdi-vue/v3'
+import {
+    mdiHome,
+    mdiClipboardTextOutline,
+    mdiCalendarEdit,
+    mdiCalendar,
+    mdiTextBoxPlusOutline,
+    mdiTextBoxOutline,
+    mdiAccount
+}  from '@mdi/js'
+
 import './styles/app.css'
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(mdiVue, {
+        icons: {
+            mdiHome,
+            mdiClipboardTextOutline,
+            mdiCalendarEdit,
+            mdiCalendar,
+            mdiTextBoxPlusOutline,
+            mdiTextBoxOutline,
+            mdiAccount
+        }
+    })
+    .mount('#app')
