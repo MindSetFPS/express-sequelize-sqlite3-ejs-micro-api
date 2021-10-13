5fcbefa7a12e9242360e56ca4008ea40ac32cead
Daniel <daanielescobar@gmail.com>
2021-10-12 21:47:45 -0500

    Rework on navbar.

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,51 +1,35 @@
 <template>
-    <nav class="bg-blue-200 w-1/12 sticky  h-screen flex flex-col items-center justify-evenly left-0  top-0" > 
-        <router-link to="/" class="" > 
-            <span class="material-icons" >&#xe88a;</span>
-            <p class="hidden" > Inicio </p> 
-        </router-link>
-        <router-link to="/pedidos" > 
-            <span class="material-icons">&#xef6e;</span>
-            <p class="hidden" >Pedidos</p>
-        </router-link>
-
-        <router-link to="/calendar/create-menu" >
-            <span class="material-icons">&#xe561;</span>
-            <p class="hidden" > Crear un menu  </p>
-        </router-link>
-
-
-        <router-link to="/calendar/list" > 
-            <span class="material-icons">&#xe8df;</span>
-            <p class="hidden" >Calendario</p>
-        </router-link>
-
-        <router-link to="/food" >
-            <span class="material-icons block">&#xea20;</span>
-            <p class="hidden" > Nuevo <br/> Platillo  </p>
-        </router-link>
-
-        <router-link to="/food/list" >
-            <span class="material-icons">&#xe0ee;</span>
-            <p class="hidden" >Platillos </p>
-        </router-link>
-
-        <router-link to="/accounts">
-            <span class="material-icons" >&#xe853;</span>
-            <p class="hidden" >Cuentas</p>
-        </router-link>
-
+    <nav 
+    class="
+        bg-blue-100 h-screen w-1/12 sticky text-center  flex flex-col items-center justify-evenly left-0  top-0 " > 
+        <navbar-item v-for="item in pages" :key="item.name" :text="item.name" :icon="item.icon"  :to="item.link" class="mx-auto text-center" />
     </nav>
 </template>
-
 <script>
+import NavbarItem from './NavbarItem.vue'
+
+
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    components: {
+        NavbarItem,
+    }, 
+    data(){
+        return{
+            pages: [
+                {name: 'Inicio',         icon: 'home', link: '/'},
+                {name: 'Pedidos',        icon: 'clipboard-text-outline', link: '/pedidos'},
+                {name: 'Crear Menu',     icon: 'calendar-edit', link: '/calendar/create-menu'},
+                {name: 'Calendario',     icon: 'calendar', link: '/calendar/list'},
+                {name: 'Nuevo Platillo', icon: 'text-box-plus-outline', link: '/food'},
+                {name: 'Platillos',      icon: 'text-box-outline', link: '/food/list'},
+                {name: 'Cuentas',        icon: 'account', link: '/accounts'},
+            ]
+        }
+    }
 }
 </script>
 
 <style scoped>
-    .lateral-nav{
-        position: fixed;
-    }
+
 </style>
---
+++ frontend/src/components/NavbarItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <router-link
+        :to="link" 
+        class="text-blue-400 hover:text-blue-700 w-full"
+    >
+        <mdicon :name="icon" />
+        <p class="hidden text-sm lg:block" >{{ text }}</p>
+    </router-link>
+</template>
+<script>
+export default {
+    name: 'NavbarItem',
+    props: ['text', 'icon', 'link']
+}
+</script>
+<style>
+    svg {
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
