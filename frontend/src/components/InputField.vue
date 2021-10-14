b935341aa0b647fdc02862764d30e915dadf488f
Daniel <daanielescobar@gmail.com>
2021-10-13 20:47:33 -0500

    Create InputFieldComponent

---
+++ frontend/src/components/InputField.vue
@@ -0,0 +1,39 @@
+<template> 
+    <div :class="`w-full transition ease-in  ${[focus ? ' mt-0 duration-500 ' : ' -mt-4 ease-in-out duration-500' ]} `" >
+        <label
+            :class="
+                `transition  text-xs p-0
+                ${[focus ? 'opacity-100 duration-400 ease-in' : 'opacity-0 duration-100 ease-in' ]}`"
+        >
+            {{ placeholder }}
+        </label>
+        <input
+            class="p-2 rounded-md w-full "
+            :placeholder="placeholder"
+            :type="inputType"
+            :value="modelValue"
+            :autocomplete="autocomplete"
+            :list="list"
+            @input="$emit('update:modelValue', $event.target.value)"
+            @focus="focus = true"
+            @blur="focus = false"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            focus: false,
+        }
+    },
+    props: {
+        placeholder: String,
+        modelValue: String,
+        inputType: String,
+        autocomplete: String,
+        list: String
+    },
+};
+</script>
