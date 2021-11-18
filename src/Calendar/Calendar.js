000e47a5583b15dfe86d7627c86eaa9b31460d56
Daniel Escobar <daanielescobar@gmail.com>
2021-11-18 12:59:35 -0600

    Rename file.

--- src/Calendar/Calendar.js
+++ src/Calendar/Calendar.js
@@ -7,83 +7,6 @@ const { Op } = require('sequelize')
 const Food  = require('../Food/FoodModel')
 const Calendar = require('./CalendarModel')
 
-router.get('/create-menu', (req, res) => {
-    Food.findAll().then(
-        food => {
-            if(Array.isArray(food) && food.length){
-                res.render('create-today-menu', {menu: food})
-            } else {
-                res.render('create-today-menu', {menu: food, error: 'Para crear un Menu,  primero tienes que agregar al menos 2 platillos.'})
-            }
-        }
-    )
-})
-
-router.post('/create-menu', async (req, res) => {
-    if(req.body.comida1 && req.body.comida2){
-
-        const search1 = await Food.findOne({
-            where: {
-                name: req.body.comida1
-            }
-        }).catch( e => console.error(e))
-        
-        const search2 = await Food.findOne({
-            where: {
-                name: req.body.comida2
-            }
-        }).catch( e => console.error(e))
-
-        console.log('Id comida1: ' + search1)
-        console.log('Id comida2: ' + search2.id)
-
-        newTodayMenu = Calendar.build({
-            comida1: search1.id,
-            comida2: search2.id,
-            date: req.body.date
-        })
-        await newTodayMenu.save().then( (e) => {
-            console.log(e)
-            res.redirect('/')
-        })
-        
-    }
-})
-
-
-router.get('/list', (req, res)=> {
-    formatedDate = date().format('YYYY-MM-DD')
-    Calendar.findAll({
-        where: {
-            date: {[Op.gte]: formatedDate}
-        },
-        include: [
-            { model: Food },
-            { model: Food }
-        ]
-    }).then(
-        menuList => {
-            if(Array.isArray(menuList) && menuList.length){  
-                const clientMenuList = []
-                menuList.forEach(element => {
-                    const x = {
-                        fecha : date(element.date).locale('es').format('LL'),
-                        comida1 : element.Comida1,
-                        comida2 : element.Comida2,
-                        id: element.id
-                    }
-                    clientMenuList.push(x)
-                })
-                
-                console.log(clientMenuList)
-                
-                return res.render('calendar', {menuList: clientMenuList, date: date().locale('es').format('LL')})
-            }
-                res.render('calendar', {date: date().locale('es').format('LL'), error: 'Crea tu primer Menu.'})
-        }
-    ).catch( e => console.log(e) )
-})
-
 router.get('/api/all', async (req, res) => {
     console.log('New request for /calendar/api/all')
     formatedDate = date().format('YYYY-MM-DD')
@@ -103,7 +26,6 @@ router.get('/api/all', async (req, res) => {
     }else{
         console.log(calendars)
     }
-
     res.json(calendars)
 })
 
@@ -129,9 +51,7 @@ router.get('/api/list', async (req, res) => {
     if(calendar){
         console.log('Correcto')
         res.json({ok: true, message: 'Ok', calendar: calendar})
-    }
-    
-
+    }    
 })
 
 router.post('/api/create', async (req, res) => {
@@ -147,8 +67,7 @@ router.post('/api/create', async (req, res) => {
             name: req.body.comida1
         }
     }).catch( e => console.error(e))
-
-    
+  
     const search2 = await Food.findOne({
         where: {
             name: req.body.comida2
@@ -194,9 +113,7 @@ router.get('/api/delete/:id', async (req, res) => {
     if(!menu){
         return res.status(404).json({ok: false, message: 'No encontrado'})
     }
-
     res.json({ok: true, message: 'Succesfully deleted'})
 })
-
 
 module.exports = router;
