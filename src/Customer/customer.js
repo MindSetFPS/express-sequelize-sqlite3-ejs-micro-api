0e82a9abcbc34c477ac4340a0cb0715c48efe246
Daniel Escobar <daanielescobar@gmail.com>
2022-01-01 13:17:39 -0600

    Get food from every pedido.

--- src/Customer/customer.js
+++ src/Customer/customer.js
@@ -1,19 +1,29 @@
 const router = require('express').Router()
 const Customer = require('./CustomerModel')
-const { Pedido } = require('../Pedido/PedidoModel')
+const { Pedido, PedidoItems } = require('../Pedido/PedidoModel')
 const Food = require('../Food/FoodModel')
 
-
 router.get('/all', async (req, res) => {
-    console.log('New request for /customer/all')
-    const customers = await Customer.findAll().catch( e => console.error(e))
+    console.log('New re quest for /customer/all')
+    const customers = await Customer.findAll({
+        include: {
+            model: Pedido,
+            attributes: ['id'],
+            include: {
+                model: Food,
+                attributes: ['id', 'name'],
+                through: {
+                    attributes: ['quantity']
+                }
+            }
+        }
+    }).catch(e => console.error(e))
     res.json(customers)
 })
 
 router.get('/:id', async (req, res) => {
     console.log('New request for /customer/:id')
     console.log(req.params)
-    
     
     const customer = await Customer.findOne({
         where: {
@@ -28,12 +38,9 @@ router.get('/:id', async (req, res) => {
         },
         include:{
             model: Food,
-            through: {
-                attributes: ['quantity']
-            }
+            attributes: ['quantity']
         }
     })
-
     console.log(customer)
     res.json({ok: true, customer: customer, pedidos: pedidos})
 })
